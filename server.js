56c41b278588ed2a347892634cc3de312081d2ec
sofia abbas <abbassofia92@gmail.com>
2022-12-02 13:13:55 -0600

    Express Routing

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import config from "./config";
 // TODO: import router from routes/
+import apiRouter from "./routes";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("common"));
 
 // TODO: use the imported router to handle all requests
+app.use("/api", apiRouter);
 
 app.use((err, req, res, next) => {
   console.error(err);
